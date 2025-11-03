af60cf0b69ca60ae4c0e94cf3f8cc8744e144c66
Guilherme Trajano <guilherme.trajano@urano.pro>
2025-11-03 00:27:31 -0300

    vercel test fix = 1

--- frontend/src/services/tmdb.js
+++ frontend/src/services/tmdb.js
@@ -2,12 +2,12 @@ import { api } from './http'
 
 export const TMDb = {
   popular(page = 1) {
-    return api.get(`/tmdb/popular`, { params: { page } })
+    return api.get(`/api/tmdb/popular`, { params: { page } })
   },
   details(id) {
-    return api.get(`/tmdb/movie/${id}`)
+    return api.get(`/api/tmdb/movie/${id}`)
   },
   search(q, page = 1) {
-      return api.get('/tmdb/search', { params: { q, page } })
+      return api.get('/api/tmdb/search', { params: { q, page } })
   }
 }
